4f71bf6d3f1ed328244b1e88817753fede017ae3
Girish Rawat <girishrawat@Girishs-MacBook-Air.local>
2015-10-02 01:01:33 +0530

    Why is this not working?

--- upload.php
+++ upload.php
@@ -1,19 +1,38 @@
 <?php 
 
 $target_dir = "uploads/";
-
-
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+$servername = "localhost";
+$username = "root";
+$password = "piedpiper";
+$dbname = "questions";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO quest (question_no, subgroup, question, answer, file)
+VALUES ('$_POST[question_no]','$_POST[subgroup]', '$_POST[question]', '$_POST[answer]', '$target_file')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
-
 
 // Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
+
 
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded. Ended here!";
@@ -25,5 +44,6 @@ if ($uploadOk == 0) {
         echo "Sorry, there was an error uploading your file.";
     }
 }
+
 
 ?>
